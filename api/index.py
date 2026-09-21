46fc2f9b381ff0c8ef740c90f708cabf9db821fb
Farhan Attar <attarfarhan02@gmail.com>
2026-09-21 20:32:26 +0530

    Fix rewrite destination to /api/index and export clean WSGI app

--- api/index.py
+++ api/index.py
@@ -1,12 +1,9 @@
 import os
 import sys
 
-# Ensure root directory is in sys.path
+# Ensure project root directory is in sys.path
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if BASE_DIR not in sys.path:
     sys.path.insert(0, BASE_DIR)
 
 from app import app
-
-if __name__ == "__main__":
-    app.run()
